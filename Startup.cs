9afb440f030a7ecd8f8a1badb61469544c6a3132
hoang-hung-hd <hoangvanhunghd96@gmail.com>
2022-05-09 17:29:02 +0700

    end basic

---
+++ Startup.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using App.ExtendMethods;
+using App.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace App
+{
+    public class Startup
+    {
+         public static string contentRootPath {set;get;}
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            contentRootPath = env.ContentRootPath;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            /*
+            Thiet lap View luu theo cac thu muc rieng nhu duoi, trong do :
+            {0} : Ten Action
+            {1} : Ten Controller
+            {2} : Teen Area
+            */
+            services.Configure<RazorViewEngineOptions>(options =>{
+                options.ViewLocationFormats.Add("/MyView/{1}/{0}" + RazorViewEngine.ViewExtension);
+            });
+
+            services.AddSingleton<ProductService>();
+            services.AddSingleton<PlanetService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.AddStatusCodePage();
+
+            app.UseStaticFiles();
+
+            app.UseStatusCodePages();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapAreaControllerRoute(
+                    name: "product",
+                    areaName : "product",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+                
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
